4912ecbe7b596186b533357b102ed783b1d08eb3
Otavio Augusto <otavioaugust@gmail.com>
2022-06-28 14:12:14 -0300

    version 1.1

--- js/Cancelar.js
+++ js/Cancelar.js
@@ -1,3 +1,42 @@
 
+function salvarMensagem(){ // mensargem de cancelamento
+
+}
+
+
+function pesquisar(){ //montar a array para cancelar
+
+}
+
+function lista(){ // visualizar a array
+    let listaCodSol = [0,2,4]
+
+}
+
+function play(){ // iniciar o cancelamento
+
+    let url = `https://sisregiii.saude.gov.br/cgi-bin/gerenciador_solicitacao?etapa=CANCELAR_SOLICITACAO&co_seq_solicitacao=${listaCodSol[index]}"&st_situacao=C&ds_observacao="${justifica}`
+
+
+} 
+    
+
+function stop(){ // parar a execução
+
+
+}
+
+function reset() { // limpar a array
+
+
+}
+
+function mais(){ // mais dias para a pesquisa 
+
+}
+
+function menos(){ // menos dias para a pesquisa
+
+}
 
 
